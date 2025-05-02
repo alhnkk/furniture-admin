14f506e823342d6bd8d60260464692dd40380186
Alihan Küçükkaya <alhnkk@gmail.com>
2025-05-02 14:08:35 +0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <Button className="p-4" variant="link">
+        Giriş Yap
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
